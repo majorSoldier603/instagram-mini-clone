231829a71c4d02601d328e6148708d1a7f412b38
Major Soldier <majorsoldier603@proton.me>
2024-06-06 16:40:30 +0200

    god damit

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 async function init() {
-	console.log("init")
-	checkLocalStorage();
-	await fetchData();
-	loadingStorys(DefaultData);
-}
+	console.log("Try init")
+	if (localStorage.UserData) {
+		console.log("Local Storage was found, Loading it ...")
 
-function checkLocalStorage() {
+	} else {
+		console.warn("Local Storage was not found, Loading default ...")
+		initLocalStorage();
+	}
 	
 }
 
-let DefaultData;
+function DEL() {
+	localStorage.clear();
+	console.log(localStorage)
+}
+
+async function initLocalStorage() {
+	await fetchData();
+	loadingStorys();
+}
 
 async function fetchData() {
 	let response = await fetch("http://localhost:5500/defaultData.json");
 	let defaultData = await response.json();
 	console.log(defaultData);
-	DefaultData = defaultData;
+	setNewlocalStorage(defaultData);
+}
+
+function setNewlocalStorage(defaultData) {
+	console.log(defaultData)
+	localStorage.setItem('UserData', JSON.stringify(defaultData.UserData))
 }
 
 function renderit(id, html) {
-	document.getElementById(id).innerHTML += html
+	id.innerHTML = ''
+	id.innerHTML += html
 }
 
-function loadingStorys(DefaultData) {
-	console.log(DefaultData)
+function loadingStorys() {
+	const LoopData = JSON.parse(localStorage.getItem("UserData") || "[]");
+	console.log(LoopData)
+
+	for (let index = 0; index < LoopData.length; index++) {
+		let profileImg = localStorage.UserData[index]
+		console.log(localStorage)
+		console.log(profileImg)
+		renderit(storys, `<div><img src="${profileImg}" alt="" srcset=""><h5>${localStorage.UserData[index].profileimg}</h5></div>`)
+	}
 }
